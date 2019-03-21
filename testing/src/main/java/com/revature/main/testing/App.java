fb6f74fdca30c7e556767192921d4798bd912f3e
J Shim <shimjay1@gmail.com>
2019-03-21 11:30:57 -0400

    blank maven project

---
+++ testing/src/main/java/com/revature/main/testing/App.java
@@ -0,0 +1,13 @@
+package com.revature.main.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
